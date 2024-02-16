eb27cb7873eacf7efee79355dae71d3c61e85b7b
HwangJungeon <maker@jungeon.cc>
2024-02-16 14:37:35 +0900

    모델 로딩 상태 표시 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
+import { useState, useEffect, useRef } from 'react';
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
 import { OrbitControls, Environment, Clone } from "@react-three/drei";
 import { useGLTF } from "@react-three/drei";
 
@@ -9,22 +9,33 @@ const Models = [
   { name: "Model_Name", url: "/models/pico-sonic-assemble-guide.glb" },
 ];
 
-const Model = ({ url }) => {
-  const { scene } = useGLTF(url);
+const Model = ({ url, onLoaded }) => {
+  const { scene } = useGLTF(url, undefined, undefined, (e) => {
+    onLoaded();
+  });
   return <Clone object={scene} position={[0, -0.01, 0]} />;
 };
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
   const controls = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // Adjust this value to simulate the time it takes to load your model
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="w-full h-screen">
+      {isLoading && <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">모델 로드 중..</div>}
       <Canvas camera={{ position: [0.1, 0.1, 0.1], near: 0.025 }}>
         <Environment preset="forest" />
-        <Suspense>
-          <Model url={Models[0].url} />
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 10]} />
-        </Suspense>
+        <Model url={Models[0].url} onLoaded={() => setIsLoading(false)} />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
         <OrbitControls
           ref={controls}
           minPolarAngle={0}
